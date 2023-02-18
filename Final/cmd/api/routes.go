6fbbbbb9568bf5604c40824d319eff696877546c
Diamondw69 <almazsydykov768@gmail.com>
2023-02-18 12:52:41 +0600

    patch 0.1

--- Final/cmd/api/routes.go
+++ Final/cmd/api/routes.go
@@ -10,12 +10,8 @@ func (app *application) routes() http.Handler {
 	router := mux.NewRouter()
 	fs := http.FileServer(http.Dir("./cmd"))
 	router.PathPrefix("/cmd/").Handler(http.StripPrefix("/cmd/", fs))
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "cmd/api/templates/indexx.html")
-	}).Methods("GET", "OPTIONS")
-	router.HandleFunc("/reg", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "cmd/api/templates/reg.html")
-	}).Methods("GET", "OPTIONS")
+	router.HandleFunc("/", app.mainPageHandler).Methods("GET", "OPTIONS")
+	router.HandleFunc("/reg", app.RegPageHandler).Methods("GET", "OPTIONS")
 	router.HandleFunc("/case", app.casePageHandler).Methods("GET", "OPTIONS")
 	return app.recoverPanic(app.rateLimit(router))
 
